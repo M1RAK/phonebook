21a4d31c2129046be93b36f47bcb7ea96e50a84e
M1RAK <abdullahiismail1105@gmail.com>
2024-09-15 15:02:11 +0100

    part03 completed

--- index.js
+++ index.js
@@ -1,122 +1,113 @@
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+const Person = require('./models/person')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) {
-	return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
-let persons = [
-	{
-		id: '1',
-		name: 'Arto Hellas',
-		number: '040-123456'
-	},
-	{
-		id: '2',
-		name: 'Ada Lovelace',
-		number: '39-44-5323523'
-	},
-	{
-		id: '3',
-		name: 'Dan Abramov',
-		number: '12-43-234345'
-	},
-	{
-		id: '4',
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122'
-	}
-]
-
-const generateId = () => {
-	const id = Math.floor(Math.random() * 199)
-	return String(id)
-}
-
-const validatePerson = (name, number) => {
-	let response = { isValid: true, errorMsg: '' }
-
-	if (!name || !number) {
-		response = {
-			isValid: false,
-			errorMsg: 'Please fill in the required information...'
-		}
-	}
-
-	persons.find((person) => {
-		if (person.name === name) {
-			response = { isValid: false, errorMsg: 'name must be unique' }
-		} else if (person.number === number) {
-			response = { isValid: false, errorMsg: 'number must be unique' }
-		}
-	})
-	return response
-}
-
-app.get('/info', (req, res) => {
-	const date = new Date()
-	res.send(
-		`<p>Phonebook has info for ${persons.length} ${
-			persons.length > 1 ? 'persons' : 'person'
-		} <br /> ${date}</p>`
-	)
-})
-
+let PERSONS = []
 app.get('/api/persons', (req, res) => {
-	res.json(persons)
+  Person.find({}).then((persons) => {
+    PERSONS.push(persons)
+    return res.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-	const id = req.params.id
-	persons = persons.find((person) => person.id === id)
-	if (persons) {
-		res.json(persons)
-	} else {
-		res.status(404).end('<h1>Person not found...</h1>')
-	}
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end('<h1>Person not found...</h1>')
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.post(
-	'/api/persons',
-	morgan(':method :url :status :res[content-length] - :response-time :body'),
-	(req, res) => {
-		const { name, number } = req.body
-		const { isValid, errorMsg } = validatePerson(name, number)
+app.post('/api/persons', (req, res, next) => {
+  const { name, number } = req.body
 
-		if (!isValid) {
-			return res.status(400).json(errorMsg)
-		}
+  const person = new Person({
+    name,
+    number
+  })
 
-		const person = {
-			id: generateId(),
-			name,
-			number
-		}
+  person
+    .save()
+    .then((savedPerson) => res.json(savedPerson))
+    .catch((error) => next(error))
+})
 
-		persons = persons.concat(person)
-		return res.json(person)
-	}
-)
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+  const id = req.params.id
 
-app.delete('/api/persons/:id', (req, res) => {
-	const id = req.params.id
-	persons = persons.filter((person) => person.id !== id)
-	res.status(204).end()
+  const person = {
+    name,
+    number
+  }
+
+  const options = {
+    new: true,
+    runValidators: true,
+    context: query
+  }
+
+  Person.findByIdAndUpdate(id, person, options)
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+
+  Person.findByIdAndDelete(id)
+    .then((person) => res.json(person).status(204).end())
+    .catch((error) => next(error))
+})
+
+app.get('/api/info', (req, res) => {
+  const date = new Date()
+  const personNo = PERSONS[0]
+
+  res.send(
+    `<h2>Phonebook has information for ${personNo?.length || 0} ${
+      personNo?.length > 1 ? 'persons' : 'person'
+    } <br /> ${date}</h2>`
+  )
 })
 
 app.use(
-	morgan('dev', {
-		skip: function (req, res) {
-			return res.statusCode < 400
-		}
-	})
+  morgan('dev', {
+    skip: function (req, res) {
+      return res.statusCode < 400
+    }
+  })
 )
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}...`))
